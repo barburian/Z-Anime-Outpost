14254cc77cf632dde95865bae59d09df2d6aed6b
barburian <62851159+barburian@users.noreply.github.com>
2026-01-05 12:43:44 +0200

    DEV12 (#2)
    * DEV12
    
    Feat:
    Smooth Camera Follow
    Hybrid Input Manager
    Physics-Driven Player Movement
    
    * Dev12
    
    MAde it better
    
    * dev12
    
    made even better

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField]private SpriteRenderer mapRenderer;
+    public float movSpeed;
+    private float _minX, _maxX, _minY, _maxY;
+    private Vector2 moveInput;
+    
+    Rigidbody2D rb;
+    void Start()
+    {   
+        float playerWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        float playerHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
+
+
+        rb = GetComponent<Rigidbody2D>();
+        _minX = mapRenderer.bounds.min.x;
+        _maxX = mapRenderer.bounds.max.x;
+        _minY = mapRenderer.bounds.min.y;
+        _maxY = mapRenderer.bounds.max.y;
+        
+        _minX += playerWidth; _maxX -= playerWidth;
+        _minY += playerHeight; _maxY -= playerHeight;
+    }
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+     void FixedUpdate()
+    {
+        Vector2 nextPosition = rb.position + moveInput * movSpeed * Time.fixedDeltaTime;
+
+        float clampedX = Mathf.Clamp(nextPosition.x, _minX, _maxX);
+        float clampedY = Mathf.Clamp(nextPosition.y, _minY, _maxY);
+
+        rb.MovePosition(new Vector2(clampedX, clampedY));
+    
+    }
+}
